0d93bd85fdd99de56f125b9e6527924f0aeea53b
t3chn0tr0n <technotron.avik@gmail.com>
2018-06-28 16:17:07 +0530

    add sqlite3 to do magic and remove any _pickle traces - Files are now save in a database - UI is still filthy as hell - tags are save in DB, however, not used - soon the use of tags will be implemented

--- tmux.py
+++ tmux.py
@@ -1,5 +1,5 @@
-from texttable import Texttable
-import _pickle as FileTruck
+import sqlite3
+from pathlib import Path
 
 
 class Bookmark:
@@ -17,7 +17,7 @@ class BookmarkSystem:
     tags = {}
     ids = {}
 
-    def retrieve_data(self):
+    def retrieve_data(self, c):
         """
         This method restores data from disk to the program - just the reverse of store data
         It uses cpickle or rather _pickle(renamed version for py3) to load the data-structure
@@ -25,19 +25,13 @@ class BookmarkSystem:
         """
         pass
 
-    def store_data(self):
-        """
-        This method restores data from disk to the program - just the reverse of store data
-        It uses cpickle or rather _pickle(renamed version for py3) to dump the data-structure
-        :return: None
-        """
-        pass
-
+    @property
     def id_creator(self):
         """
         creates a New Id for a new Bookmark
         :return:
         """
+        self.last_allocated_id_no = 0
         new_id = self.last_allocated_id_no + 1
         self.last_allocated_id_no += 1
         return new_id
@@ -54,54 +48,72 @@ class BookmarkSystem:
         """
         pass
 
-    def display_list(self):
+    def display_list(self, table):
         """
         The name is pretty self explanatory
         This method prints a list of all saved bookmarks
         :return:
         """
         print("\nWELCOME TO TMARK: A customisable Terminal Bookmarking System")
-        print("================  ------------------------------------------")
-        print()
-        for bookamrks in self.list_by_name:
-            temp_bookmark = self.list_by_name[bookamrks]
-            print("ID\t\t:", temp_bookmark.id)
-            print("NAME\t:", temp_bookmark.name)
-            print("URL\t\t:", temp_bookmark.url)
-            print("TAGS\t:", temp_bookmark.tags)
-            print("\n------------------------------------------------------------------------------")
-        else:
+        print("================  ------------------------------------------\n")
+
+        try:
+            table.execute("SELECT * FROM bookmarks")
+            bookmark = table.fetchall()
+            flag = 0
+            for data in bookmark:
+                flag = 1
+                print("ID\t\t:", data[0])
+                print("NAME\t:", data[1])
+                print("URL\t\t:", data[2])
+                table.execute("SELECT tag FROM tags WHERE bookmark_id IS ?", data[0][0])
+                tags = table.fetchall()
+                print("TAGS\t:", data[0])
+                for x in tags:
+                    print(x, end=" ")
+                print("\n------------------------------------------------------------------------------")
+        except sqlite3.OperationalError:
+            flag = 0
+
+        if flag == 0:
             print("You Bookmark List is empty!")
             print("To get started, just type: a_command_goes_here_not_decided_yet ")
             print("To get Help, type: >> tmark --help")
             print("For more Information/source code visit: https://github.com/t3chn0tr0n/tmux")
 
-    def create(self):
+    def create(self, table, database):
         """
         Creates a new Bookmark
         :return:
         """
         b = Bookmark()
+        table.execute("CREATE TABLE IF NOT EXISTS bookmarks(id TEXT, name TEXT, url TEXT)")
+        table.execute("CREATE TABLE IF NOT EXISTS tags(tag TEXT, bookmark_id TEXT)")
 
-        name_of_bookmark = input("Enter the name: ")
-        url_of_bookmark = input("Enter the url: ")
-        tags_for_bookmark = input("Enter tags for the bookmark, seperated by commas(,): ").split(",")
+        b.name = input("Enter the name: ")
+        b.url = input("Enter the url: ")
+        b.tags = input("Enter tags for the bookmark, separated by commas(,): ").split(",")
+        b.id = b.name[0].upper() + (str(self.id_creator).zfill(3))
 
-        b.name = name_of_bookmark
-        b.url = url_of_bookmark
-        b.id = b.name[0].upper() + (str(self.id_creator()).zfill(3))
-        b.tags = tags_for_bookmark          # this is only temporary! for alpha version
+        table.execute("INSERT INTO bookmarks(id, name, url) VALUES(?, ?, ?)", (b.id, b.name, b.url))
+        for tag in b.tags:
+            table.execute("INSERT INTO tags(tag, bookmark_id) VALUES(?, ?)", (tag.strip(), b.id))
 
-        self.list_by_name[b.name] = b
+        database.commit()
 
+
+# connecting to SQLITE DB
+database = sqlite3.connect('bookmarks.db')
+db = database.cursor()
 
 obj = BookmarkSystem()
+
 # Empty Display
-obj.display_list()
+obj.display_list(db)
 
-obj.create()
-obj.display_list()
+obj.create(db, database)
+obj.display_list(db)
 
-
-
-
+# Disconnecting from Database
+db.close()
+database.close()
